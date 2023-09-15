a649078bfac19bf1acb8cf6a0eb84f7a1e3d8b80
yutteee <yusaku_nakamura_aa@icloud.com>
2023-09-15 16:48:17 +0900

    create opponent chat layout #2

---
+++ src/features/ChatScreen/presentation/OpponentChat/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <div class="text">{{ text }}</div>
+    <span class="detail2 time">{{ time }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OpponentChatPresentation",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  position: relative;
+  width: fit-content;
+}
+
+.text {
+  background-color: var(--white);
+  border-radius: 0 8px 8px 8px;
+  padding: 8px 12px;
+  max-width: 280px;
+}
+
+.time {
+  position: absolute;
+  bottom: 24px;
+  right: -4px;
+  transform: translate(50%, 50%);
+}
+</style>
---
+++ src/features/ChatScreen/presentation/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <OpponentChatPresentation text="Hello" time="12:00" />
+</template>
+
+<script>
+import OpponentChatPresentation from "./OpponentChat/index.vue";
+export default {
+  name: "ChatScreenPresentation",
+  props: {},
+  components: { OpponentChatPresentation },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  padding: 16px;
+}
+</style>
