49a0cf53c84ba360e5d25471f98c54020fc3ba0b
yutteee <yusaku_nakamura_aa@icloud.com>
2023-09-15 14:34:29 +0900

    changed environment viet -> webpack #1

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
-import Vue from "vue";
-import { createPinia, PiniaVuePlugin } from "pinia";
+import Vue from 'vue'
+import App from './App.vue'
 
-import App from "./App.vue";
-import router from "./router";
-
-import "./assets/main.css";
-
-Vue.use(PiniaVuePlugin);
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  pinia: createPinia(),
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
